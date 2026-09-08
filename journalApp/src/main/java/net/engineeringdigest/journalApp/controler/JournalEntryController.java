24f0587f8ac1a04713d56555b9491d2a318f8ac3
Tushar24102006 <tushar2007malkar@gmail.com>
2026-09-08 09:36:28 +0530

    Fix journal REST controller

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controler/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controler/JournalEntryController.java
@@ -1,13 +1,11 @@
 package net.engineeringdigest.journalApp.controler;
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.*;
+@RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
     private Map< Long , JournalEntry> journalEntries =new HashMap<>();
